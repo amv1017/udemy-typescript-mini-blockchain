020484d69178030c7268e7d3c152f6182cc8e0d4
amv1017 <Vinokurov@bbs-c.ru>
2021-10-11 23:53:21 +0300

    initial

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Антон", age = 28, gender = "мужик";
+const sayHi = (name, age, gender) => {
+    console.log(`Привет, ${name}. Тебе ${age} лет и ты ${gender}`);
+};
+sayHi(name, age, gender);
+//# sourceMappingURL=index.js.map
