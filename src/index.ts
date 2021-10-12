830fe2fe9958bf6dae70960beb1c7aef5792045d
amv1017 <Vinokurov@bbs-c.ru>
2021-10-12 10:14:08 +0300

    Classes in TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
-const name = "Антон", age = 28, gender = "мужчина";
-
-const sayHi = (name: string, age: number, gender?: string): void => {
-  console.log(`Привет, ${name}. Тебе ${age} лет и ты ${gender}.`)
+class Human {
+  public name: string
+  public age: number
+  public gender: string
+  constructor(name: string, age: number, gender: string) {
+    this.name = name
+    this.age = age
+    this.gender = gender
+  }
 }
 
-sayHi(name, age, gender)
+const anton = new Human("Антон", 28, "мужчина")
+
+const sayHi = (person: Human): void => {
+  console.log(`Привет, ${person.name}. Тебе ${person.age} лет и ты ${person.gender}.`)
+}
+
+sayHi(anton)
 
 export {}
